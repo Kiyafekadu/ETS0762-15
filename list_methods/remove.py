64c05682e947e699249def14d0beb4fe0b4fc6f7
kiya fekadu <kiyafekadu5@gmail.com>
2025-04-05 21:35:10 +0300

    new list methods added

---
+++ list_methods/remove.py
@@ -0,0 +1,11 @@
+
+my_set = ['cat', 'dog', 'rabbit', 'horse']
+
+
+my_set.remove('rabbit')
+
+print(my_set)
+
+my_set.remove('fish')
+
+print(my_set)
---
+++ list_methods/sort.py
@@ -0,0 +1,24 @@
+# Example 1
+
+my_num = [10, 8, 3, 22, 33, 7, 11, 100, 54]
+
+my_num.sort()
+
+print(my_num)
+
+ #output
+
+# [3, 7, 8, 10, 11, 22, 33, 54, 100]
+
+
+# Example 2
+
+my_num = [10, 8, 3, 22, 33, 7, 11, 100, 54]
+
+my_num.sort(reverse=True)
+
+print(my_num)
+
+#output
+
+#[100, 54, 33, 22, 11, 10, 8, 7, 3]
